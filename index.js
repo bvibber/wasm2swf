70d1e33533258a45f930d4eb1f746fabad4acfdc
Brion Vibber <brion@pobox.com>
2019-12-31 08:28:12 -0800

    set domainMemory

--- index.js
+++ index.js
@@ -1391,10 +1391,6 @@ function convertModule(mod) {
     iinitBody.constructsuper(0);
 
     // Initialize the memory
-    //let flashsystemns = abc.namespace(Namespace.Namespace, abc.string('flash.system'));
-    //iinitBody.getlex(abc.qname(flashsystemns, abc.string('ApplicationDomain')));
-    //iinitBody.getproperty(abc.qname(pubns, abc.string('currentDomain')));
-
     iinitBody.getlocal_0();
     iinitBody.getlex(abc.qname(flashutilsns, abc.string('ByteArray')));
     iinitBody.construct(0);
@@ -1404,11 +1400,16 @@ function convertModule(mod) {
     iinitBody.dup();
     iinitBody.pushint(2 ** 24); // default to 16 MiB memory for the moment
     iinitBody.setproperty(abc.qname(pubns, abc.string('length')));
-
-    //iinitBody.dup();
     iinitBody.initproperty(abc.qname(privatens, abc.string('wasm$memory'))); // on this
 
-    //iinitBody.setproperty(abc.qname(pubns, abc.string('domainMemory'))); // on ApplicationDomain.currentDomain
+    // Set it as domain memory
+    let flashsystemns = abc.namespace(Namespace.Namespace, abc.string('flash.system'));
+    iinitBody.getlocal_0();
+    iinitBody.getlex(abc.qname(flashsystemns, abc.string('ApplicationDomain')));
+    iinitBody.getproperty(abc.qname(pubns, abc.string('currentDomain')));
+    iinitBody.getlocal_0();
+    iinitBody.getproperty(abc.qname(privatens, abc.string('wasm$memory'))); // on this
+    iinitBody.setproperty(abc.qname(pubns, abc.string('domainMemory'))); // on ApplicationDomain.currentDomain
 
     // Initialize the table
     iinitBody.getlocal_0();
